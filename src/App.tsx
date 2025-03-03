89ebd1fc92365ef3c74dc665b2d848f7c8bf6436
Rohit Jaryal <rohit.jaryal@ext.canal-plus.com>
2025-03-03 22:50:07 +0700

    chore:  test setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+/**
+ * Objectives:
+ *  1. Add Active/ Inactive check
+ *  2. Review and fix issues.
+ *  3. Optimize the code base.
+ *  4. Unit testing.
+ */
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+
+  function handleAdd() {
+    const tempTasks = tasks;
+    tempTasks.push(currentTask);
+    setTasks(tempTasks);
+    setCurrentTask("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        onChange={(event) => setCurrentTask(event.target.value)}
+        value={currentTask}
+      />
+      <button
+        type="submit"
+        className="btn btn__primary btn__lg"
+        onClick={handleAdd}
+      >
+        Add
+      </button>
+
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">{tasks.length} tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {tasks.map((task, index) => (
+          <li className="todo stack-small">
+            <div className="c-cb">
+              <input type="checkbox" />
+              <label className="todo-label" htmlFor={`todo-${index}`}>
+                {task}
+              </label>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
